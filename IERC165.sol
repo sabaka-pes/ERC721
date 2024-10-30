9c2d7c5665e07a92523ef3bcd720907582f1a36d
sabaka-pes <monsutaaisu@gmail.com>
2024-10-30 12:44:59 +0300

    Final: errors fixed

--- IERC165.sol
+++ IERC165.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 interface IERC165 {
-    function supportInterface(bytes4 interfaceId) external view returns(bool);
+    function supportsInterface(bytes4 interfaceId) external view returns(bool);
 }
